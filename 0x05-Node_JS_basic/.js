d0c9d96e8b945b4c2e5a140dd2c0021ebebde01d
root <you@example.com>
2023-07-31 05:52:30 -0700

    javascript

---
+++ 0x05-Node_JS_basic/.js
@@ -0,0 +1,9 @@
+.js  1-stdin.js  2-main_0.js  full_server  nodesource_setup.sh  README.md
+root@fe7d5cb38656:~/alx-backend-javascript/0x05-Node_JS_basic# ls
+0-console.js  0-main.js  1-stdin.js  2-main_0.js  full_server  nodesource_setup.sh  README.md
+root@fe7d5cb38656:~/alx-backend-javascript/0x05-Node_JS_basic#
+
+
+
+
+
